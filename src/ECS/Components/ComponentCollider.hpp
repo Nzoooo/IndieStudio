3e7dfdded88cd04502c2046c6676e4c89679321b
Laurent Enzo <enzo.laurent@epitech.eu>
2022-05-25 09:56:37 +0200

    MODIF: remove isCollidable method in ComponentCollider

--- src/ECS/Components/ComponentCollider.hpp
+++ src/ECS/Components/ComponentCollider.hpp
@@ -9,17 +9,14 @@
 
 class ComponentCollider {
     public:
-        ComponentCollider(bool takeCollider = false, bool isCollidable = true) : _takeCollider(takeCollider), _isCollidable(isCollidable) {};
+        ComponentCollider(bool takeCollider = false) : _takeCollider(takeCollider) {};
         ~ComponentCollider() = default;
 
         bool getTakeCollider() const;
-        bool getIsCollidable() const;
 
         void setTakeCollider(bool takeCollider);
-        void setIsCollidable(bool isCollidable);
 
     protected:
     private:
         bool _takeCollider;
-        bool _isCollidable;
 };

c2a0fbfd41d2adb73d700f5465168d1652aff0bc
matthis <matthis.lesur@epitech.eu>
2022-05-30 10:32:14 +0200

    [ADD] Setting scene

---
+++ src/core/settingsMenu.hpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2022
+** B-YEP-400-NAN-4-1-indiestudio-matthis.lesur
+** File description:
+** settingsMenu
+*/
+
+#ifndef SETTINGSMENU_HPP_
+#define SETTINGSMENU_HPP_
+
+#include "../raylib/include/Window.hpp"
+#include "../raylib/include/Font.hpp"
+#include "../raylib/include/Vector2.hpp"
+#include "../raylib/include/Mouse.hpp"
+#include "../raylib/include/Texture.hpp"
+
+int settingMenu();
+
+#endif /* !SETTINGSMENU_HPP_ */

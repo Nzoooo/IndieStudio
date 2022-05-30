d43130864bedf1f4fee2dee6f57d02b504d1ede4
matthis <matthis.lesur@epitech.eu>
2022-05-30 10:33:56 +0200

    [UPT] add switchcase to select scene

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,38 @@
 
 #include "core/core.hpp"
 #include "core/mainMenu.hpp"
+#include "core/settingsMenu.hpp"
+
+int mainLoop()
+{
+    int res = 0;
+    
+    while (res != -1) {
+        switch (res)
+        {
+        case 0:
+            res = mainMenu();
+            break;
+        case 1:
+            res = coreLoop();
+            break;
+        case 2:
+            // res = reload();
+            break;
+        case 3:
+            res = settingMenu();
+            break;
+        case -1:
+            return (-1);
+        }
+    }
+    return (0);
+}
 
 int main(int argc, char **argv)
 {
     (void)argc;
     (void)argv;
-    mainMenu();
-    // coreLoop();
+    mainLoop();
     return 0;
 }

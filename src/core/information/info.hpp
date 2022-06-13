39f4ea4d6b3e2ccadc704302f46dcc20691e5bd4
BELLICHA Tanguy <tanguy.bellicha@epitech.eu>
2022-06-13 09:50:21 +0200

    [ADD] Browse all entities

---
+++ src/core/information/info.hpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2022
+** Project
+** File description:
+** info
+*/
+
+#pragma once
+
+#include "ECS/Components/ComponentCube.hpp"
+#include "ECS/Components/ComponentDrawable.hpp"
+#include "ECS/Entities/IEntity.hpp"
+#include "ECS/ecs.hpp"
+
+void displayInformations(ecs::Core &core);

8c20ed4a149c5b1cfc4ed23dedf56058d6c92f08
BELLICHA Tanguy <tanguy.bellicha@epitech.eu>
2022-06-13 10:40:41 +0200

    [UPT] Add all include header files

--- src/load/loadComponent.hpp
+++ src/load/loadComponent.hpp
@@ -8,8 +8,14 @@
 #ifndef LOADCOMPONENT_HPP_
 #define LOADCOMPONENT_HPP_
 
+#include "ECS/Components/ComponentClickable.hpp"
+#include "ECS/Components/ComponentCollider.hpp"
+#include "ECS/Components/ComponentDrawable.hpp"
+#include "ECS/Components/ComponentDroppable.hpp"
+#include "ECS/Components/ComponentExplodable.hpp"
 #include "ECS/Components/ComponentKillable.hpp"
 #include "ECS/Components/ComponentMovable.hpp"
+#include "ECS/Components/ComponentPickable.hpp"
 #include "ECS/Components/ComponentTransform.hpp"
 #include "ECS/Entities/IEntity.hpp"
 

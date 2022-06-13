85a5d972d32f166ac14040c318cd95c5e9e2b3ce
BELLICHA Tanguy <tanguy.bellicha@epitech.eu>
2022-06-13 15:09:52 +0200

    [UPT] Add background behind each information

--- src/core/information/info.cpp
+++ src/core/information/info.cpp
@@ -7,28 +7,67 @@
 
 #include "info.hpp"
 
-static ecs::IEntity *createBackground()
-{
-    ecs::IEntity *entity = new ecs::IEntity();
+#define WINDOW_WIDTH         800
+#define WINDOW_HEIGHT        600
+#define DEFAULT_TRANSPARENCY 128
+#define WIDTH_BACKGROUND     224
+#define HEIGHT_BACKGROUND    128
+#define MARGIN_BORDER        16
+#define ICON_SIZE            32
 
-    raylib::Vector3 position(10.0f, -10.0f, 10.0f);
-    raylib::Vector3 size(10.0f, 1.0f, 10.0f);
-    entity->add<ComponentDrawable>(true, false);
-    entity->add<ComponentTransparency>(200);
-    entity->add<ComponentRectangle>(10, 10, 200, 100);
-    entity->get<ComponentRectangle>()->setColor(entity->get<ComponentTransparency>()->sumWithColor(raylib::Color::Blue()));
-    return (entity);
+static raylib::Color mainPlayersColor[4] = {
+    raylib::Color::Black(),
+    raylib::Color::White(),
+    raylib::Color::Red(),
+    raylib::Color::Blue(),
+};
+
+static ecs::IEntity *createBackground(bool TopBottom, bool LeftRight, raylib::Color color)
+{
+    ecs::IEntity *background = new ecs::IEntity();
+    size_t margin_top = 0;
+    size_t margin_left = 0;
+
+    if (!TopBottom)
+        margin_top = MARGIN_BORDER;
+    else
+        margin_top = WINDOW_HEIGHT - (HEIGHT_BACKGROUND + MARGIN_BORDER);
+    if (!LeftRight)
+        margin_left = MARGIN_BORDER;
+    else
+        margin_left = WINDOW_WIDTH - (WIDTH_BACKGROUND + MARGIN_BORDER);
+    background->add<ComponentDrawable>(true, false);
+    background->add<ComponentTransparency>(DEFAULT_TRANSPARENCY);
+    background->add<ComponentRectangle>(margin_left, margin_top, WIDTH_BACKGROUND, HEIGHT_BACKGROUND);
+    background->get<ComponentRectangle>()->setColor(background->get<ComponentTransparency>()->sumWithColor(color));
+    return (background);
 }
 
 void displayInformations(ecs::Core &core)
 {
-    core.addEntity(createBackground());
+    std::size_t count = 0;
+    ecs::IEntity *players = new ecs::IEntity();
+    ecs::IEntity *bots = new ecs::IEntity();
+
+    players->add<ComponentKillable>();
+    bots->add<ComponentKillable>();
+    players->add<ComponentControllable>();
+    core.addEntity(players);
+    core.addEntity(bots);
+    core.addEntity(players);
+    core.addEntity(bots);
     for (auto &entity : core.getEntities()) {
-        // if (entity->has<ComponentControllable>()) {
-        //     // It's a player
-        // }
-        // else if (entity->has<ComponentHealth>()) {
-        //     // It's a bot
-        // }
+        if (entity->has<ComponentKillable>()) { // Change to ComponentHealth
+            // It's a player/bot
+            core.addEntity(createBackground(count / 2, count % 2, mainPlayersColor[count]));
+            core.addEntity(displayMainIcon(count / 2, count % 2));
+            if (entity->has<ComponentControllable>()) {
+                // It's a player
+            } else {
+                // It's a bot
+                core.addEntity(displayBotIcon(count / 2, count % 2));
+            }
+            count++;
+        }
     }
 }

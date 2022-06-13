353172b556c22b0d6dc76a9ceff2c324978d2d11
BELLICHA Tanguy <tanguy.bellicha@epitech.eu>
2022-06-13 10:56:21 +0200

    [UPT] Declaration of all loading functions

--- src/load/loadComponent.hpp
+++ src/load/loadComponent.hpp
@@ -19,9 +19,15 @@
 #include "ECS/Components/ComponentTransform.hpp"
 #include "ECS/Entities/IEntity.hpp"
 
-void addComponentMovable(std::ifstream &file, ecs::IEntity *entity);
-void addComponentTransform(std::ifstream &file, ecs::IEntity *entity);
+void addComponentClickable(std::ifstream &file, ecs::IEntity *entity);
+void addComponentCollider(std::ifstream &file, ecs::IEntity *entity);
+void addComponentDrawable(std::ifstream &file, ecs::IEntity *entity);
+void addComponentDroppable(std::ifstream &file, ecs::IEntity *entity);
+void addComponentExplodable(std::ifstream &file, ecs::IEntity *entity);
 void addComponentKillable(std::ifstream &file, ecs::IEntity *entity);
+void addComponentMovable(std::ifstream &file, ecs::IEntity *entity);
+void addComponentPickable(std::ifstream &file, ecs::IEntity *entity);
+void addComponentTransform(std::ifstream &file, ecs::IEntity *entity);
 std::string removeTabs(std::string line);
 
 #endif /* !LOADCOMPONENT_HPP_ */

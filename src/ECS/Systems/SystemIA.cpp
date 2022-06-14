3ad1ae6776192df146609270ba4168d6d18e96bb
Laurent Enzo <enzo.laurent@epitech.eu>
2022-06-14 11:15:35 +0200

    [UPDATE] start to implement collision of the ia

--- src/ECS/Systems/SystemIA.cpp
+++ src/ECS/Systems/SystemIA.cpp
@@ -7,7 +7,7 @@
 
 #include "SystemIA.hpp"
 
-SystemIA::SystemIA() : _ia(new ecs::IEntity)
+SystemIA::SystemIA(ecs::Core &core, int posX, int posY) : _ia(new ecs::IEntity), _core(core)
 {
     if (/* entoure de deux murs en x-1, y-1 || x+1, y-1 */)
         _ia->add<ComponentMovable>(ComponentMovable::Direction::DOWN, 0.1);
@@ -15,8 +15,9 @@ SystemIA::SystemIA() : _ia(new ecs::IEntity)
         _ia->add<ComponentMovable>(ComponentMovable::Direction::UP, 0.1);
     _ia->add<ComponentDrawable>(false, true);
     _ia->add<ComponentKillable>();
-    _ia->add<ComponentTransform>(50, 50, 50, 50);
+    _ia->add<ComponentTransform>(50, 50, posX, posY);
     _ia->add<ComponentCollider>();
+    _blastRange = 1;
 }
 
 SystemIA::~SystemIA()
@@ -25,18 +26,70 @@ SystemIA::~SystemIA()
         delete _ia;
 }
 
-ecs::IComponent *SystemIA::getBombInRange(ecs::IComponent *bomb) const
+bool SystemIA::checkCollisionAt(int posX, int posY)
 {
-    if ((bomb->get<ComponentTransform>()->getPosX() == _ia->get<ComponentTransform>()->getPosX() &&
-        abs(bomb->get<ComponentTransform>()->getPosY() - _ia->get<ComponentTransform>()->getPosY()) <= bomb->get<ComponentExplodable>()->getBlastRange()) ||
-        (bomb->get<ComponentTransform>()->getPosY() == _ia->get<ComponentTransform>()->getPosY() &&
-        abs(bomb->get<ComponentTransform>()->getPosX() - _ia->get<ComponentTransform>()->getPosX()) <= bomb->get<ComponentExplodable>()->getBlastRange()))
-        return (bomb);
+    for (auto *e : _core->getEntities()) {
+        if (e->has<ComponentTransform>() &&
+            e->get<ComponentTransform>()->getPosX() == posX && e->get<ComponentTransform>()->getPosY() == posY) {
+            return (true);
+        }
+    }
+    return (false);
+}
+
+void SystemIA::udpdate()
+{
+    ecs::IComponent *bomb = nullptr;
+
+    bomb = getBombInRange();
+    if (bomb)
+        setDirectionWithBomb(bomb);
+    if (isKillableBlockInRange())
+        // put bomb
+    move();
+}
+
+void SystemIA::move()
+{
+    ComponentMovable::Direction direction = _ia->get<ComponentMovable>()->getDirection();
+    float speed = _ia->get<ComponentMovable>()->getSpeed();
+
+    switch (direction) {
+        case ComponentMovable::Direction::DOWN:
+            _ia->get<ComponentTransform>()->setPosY(_ia->get<ComponentTransform>()->getPosY() + speed);
+            break;
+        case ComponentMovable::Direction::UP:
+            _ia->get<ComponentTransform>()->setPosY(_ia->get<ComponentTransform>()->getPosY() - speed);
+            break;
+        case ComponentMovable::Direction::LEFT:
+            _ia->get<ComponentTransform>()->setPosX(_ia->get<ComponentTransform>()->getPosX() - speed);
+            break;
+        case ComponentMovable::Direction::RIGHT:
+            _ia->get<ComponentTransform>()->setPosX(_ia->get<ComponentTransform>()->getPosX() + speed);
+            break;
+        default:
+            break;
+    }
+}
+
+ecs::IComponent *SystemIA::getBombInRange(std::vector<ecs::IEntity *> &entities) const
+{
+    for (auto *e : entities) {
+        if (e->has<ComponentExplodable>() && e->has<ComponentTransform>() && e->has<ComponentDroppable>()) {
+            if ((e->get<ComponentTransform>()->getPosX() == _ia->get<ComponentTransform>()->getPosX() &&
+                abs(e->get<ComponentTransform>()->getPosY() - _ia->get<ComponentTransform>()->getPosY()) <= e->get<ComponentExplodable>()->getBlastRange()) ||
+                (e->get<ComponentTransform>()->getPosY() == _ia->get<ComponentTransform>()->getPosY() &&
+                abs(e->get<ComponentTransform>()->getPosX() - _ia->get<ComponentTransform>()->getPosX()) <= e->get<ComponentExplodable>()->getBlastRange()))
+                return (e);
+        }
+    }
     return (nullptr);
 }
 
-void SystemIA::setDirectionWithBomb(ecs::IComponent *bomb)
+void SystemIA::setDirectionWithBomb(ecs::IEntity *bomb)
 {
+    if (!bomb->has<ComponentTransform>())
+        return;
     if (bomb->get<ComponentTransform>()->getPosX() == _ia->get<ComponentTransform>()->getPosX()) {
         if (bomb->get<ComponentTransform>()->getPosY() - _ia->get<ComponentTransform>()->getPosY() >= 0) {
             _ia->get<ComponentMovable>()->setDirection(ComponentMovable::Direction::UP);
--- src/ECS/Systems/SystemIA.hpp
+++ src/ECS/Systems/SystemIA.hpp
@@ -11,17 +11,23 @@
 
 class SystemIA : public ecs::ISystem {
     public:
-        SystemIA();
+        SystemIA(ecs::Core &core, int posX, int posY);
         SystemIA(const SystemIA &other) = delete;
         ~SystemIA();
         SystemIA &operator=(const SystemIA &other) = delete;
 
+        void udpdate();
 
     protected:
     private:
+        ecs::Core *_core;
         ecs::IEntity *_ia;
+        int _blastRange;
 
-        ecs::IComponent *getBombInRange(ecs::IComponent *bomb);
+        ecs::IComponent *getBombInRange(std::vector<ecs::IEntity *> &entities);
         bool isKillableBlockInRange();
-        void setDirectionWithBomb(ecs::IComponent *bomb);
+        void setDirectionWithBomb(ecs::IEntity *bomb);
+
+        void move();
+        bool checkCollisionAt(int posX, int posY);
 };

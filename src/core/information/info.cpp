d3d030db88ce9798bd27f3b8640738344f54bd74
BELLICHA Tanguy <tanguy.bellicha@epitech.eu>
2022-06-14 14:06:57 +0200

    [UPT] Change text to icon for information

--- src/core/information/info.cpp
+++ src/core/information/info.cpp
@@ -15,6 +15,7 @@
 #define HEIGHT_BACKGROUND    220
 #define MARGIN_BORDER        32
 #define ICON_SIZE            64
+#define DETAILS_ICON_SIZE    16
 #define NAME_SIZE            32
 #define DETAILS_SIZE         26
 
@@ -28,61 +29,61 @@ static raylib::Color playersColor[4] = {
 static std::string playersIcon[4] = {
     "assets/64/black_among_us.png", "assets/64/white_among_us.png", "assets/64/red_among_us.png", "assets/64/blue_among_us.png"};
 
-static ecs::IEntity *createBackground(bool TopBottom, bool LeftRight, raylib::Color color)
+static ecs::IEntity *createBackground(bool topBottom, bool leftRight, raylib::Color color)
 {
     ecs::IEntity *background = new ecs::IEntity();
-    size_t margin_top = 0;
-    size_t margin_left = 0;
+    size_t marginTop = 0;
+    size_t marginLeft = 0;
 
-    if (!TopBottom)
-        margin_top = MARGIN_BORDER;
+    if (!topBottom)
+        marginTop = MARGIN_BORDER;
     else
-        margin_top = WINDOW_HEIGHT - (HEIGHT_BACKGROUND + MARGIN_BORDER);
-    if (!LeftRight)
-        margin_left = MARGIN_BORDER;
+        marginTop = WINDOW_HEIGHT - (HEIGHT_BACKGROUND + MARGIN_BORDER);
+    if (!leftRight)
+        marginLeft = MARGIN_BORDER;
     else
-        margin_left = WINDOW_WIDTH - (WIDTH_BACKGROUND + MARGIN_BORDER);
+        marginLeft = WINDOW_WIDTH - (WIDTH_BACKGROUND + MARGIN_BORDER);
     background->add<ComponentDrawable>(true, false);
     background->add<ComponentTransparency>(DEFAULT_TRANSPARENCY);
-    background->add<ComponentRectangle>(margin_left, margin_top, WIDTH_BACKGROUND, HEIGHT_BACKGROUND);
+    background->add<ComponentRectangle>(marginLeft, marginTop, WIDTH_BACKGROUND, HEIGHT_BACKGROUND);
     background->get<ComponentRectangle>()->setColor(background->get<ComponentTransparency>()->sumWithColor(color));
     return (background);
 }
 
-static ecs::IEntity *displayPlayerHeader(std::size_t count, bool TopBottom, bool LeftRight)
+static ecs::IEntity *displayPlayerHeader(std::size_t count, bool topBottom, bool leftRight)
 {
     ecs::IEntity *entity = new ecs::IEntity();
-    raylib::Vector2 positionIcon = {MARGIN_BORDER, MARGIN_BORDER};
-    raylib::Vector2 positionText = {};
+    raylib::Vector2 iconPosition = {MARGIN_BORDER, MARGIN_BORDER};
+    raylib::Vector2 textPosition = {};
 
-    if (!TopBottom)
-        positionIcon.y = MARGIN_BORDER;
+    if (!topBottom)
+        iconPosition.y = MARGIN_BORDER;
     else
-        positionIcon.y = WINDOW_HEIGHT - (HEIGHT_BACKGROUND + MARGIN_BORDER);
-    positionText.y = positionIcon.y + MARGIN_BORDER * 0.16;
-    positionIcon.y -= ICON_SIZE / 2;
-    if (!LeftRight)
-        positionIcon.x = MARGIN_BORDER;
+        iconPosition.y = WINDOW_HEIGHT - (HEIGHT_BACKGROUND + MARGIN_BORDER);
+    textPosition.y = iconPosition.y + MARGIN_BORDER * 0.16;
+    iconPosition.y -= ICON_SIZE / 2;
+    if (!leftRight)
+        iconPosition.x = MARGIN_BORDER;
     else
-        positionIcon.x = WINDOW_WIDTH - (WIDTH_BACKGROUND + MARGIN_BORDER);
-    positionText.x = positionIcon.x + WIDTH_BACKGROUND / 3;
-    entity->add<ComponentTexture>(playersIcon[count], positionIcon);
-    entity->add<ComponentText>("assets/CollegiateBlackFLF.ttf", "PLAYER #" + std::to_string(count + 1), positionText, NAME_SIZE, raylib::Color::White());
+        iconPosition.x = WINDOW_WIDTH - (WIDTH_BACKGROUND + MARGIN_BORDER);
+    textPosition.x = iconPosition.x + WIDTH_BACKGROUND / 3;
+    entity->add<ComponentTexture>(playersIcon[count], iconPosition);
+    entity->add<ComponentText>("assets/CollegiateBlackFLF.ttf", "PLAYER #" + std::to_string(count + 1), textPosition, NAME_SIZE, raylib::Color::White());
     entity->add<ComponentDrawable>(true, false);
     return (entity);
 }
 
-static ecs::IEntity *displayBotIcon(bool TopBottom, bool LeftRight)
+static ecs::IEntity *displayBotIcon(bool topBottom, bool leftRight)
 {
     ecs::IEntity *botIcon = new ecs::IEntity();
     raylib::Vector2 position = {MARGIN_BORDER, MARGIN_BORDER};
 
-    if (!TopBottom)
+    if (!topBottom)
         position.y = MARGIN_BORDER;
     else
         position.y = WINDOW_HEIGHT - (HEIGHT_BACKGROUND + MARGIN_BORDER);
     position.y -= ICON_SIZE / 2;
-    if (!LeftRight)
+    if (!leftRight)
         position.x = MARGIN_BORDER;
     else
         position.x = WINDOW_WIDTH - (WIDTH_BACKGROUND + MARGIN_BORDER);
@@ -93,29 +94,83 @@ static ecs::IEntity *displayBotIcon(bool TopBottom, bool LeftRight)
     return (botIcon);
 }
 
-static ecs::IEntity *displayTexts(ecs::IEntity *entity, bool TopBottom, bool LeftRight)
+static ecs::IEntity *displayBombs(ecs::IEntity *entity, bool topBottom, bool leftRight)
 {
-    ecs::IEntity *texts = new ecs::IEntity();
-    raylib::Vector2 position = {MARGIN_BORDER, MARGIN_BORDER};
+    ecs::IEntity *bomb = new ecs::IEntity();
+    raylib::Vector2 iconPosition = {MARGIN_BORDER, MARGIN_BORDER};
+    raylib::Vector2 textPosition = {MARGIN_BORDER, MARGIN_BORDER};
 
-    if (!TopBottom)
-        position.y = MARGIN_BORDER;
+    if (!topBottom)
+        iconPosition.y = MARGIN_BORDER;
     else
-        position.y = WINDOW_HEIGHT - (HEIGHT_BACKGROUND + MARGIN_BORDER);
-    position.y += NAME_SIZE * 2;
-    if (!LeftRight)
-        position.x = MARGIN_BORDER;
+        iconPosition.y = WINDOW_HEIGHT - (HEIGHT_BACKGROUND + MARGIN_BORDER);
+    iconPosition.y += NAME_SIZE * 2;
+    textPosition.y = iconPosition.y + DETAILS_ICON_SIZE / 2;
+    if (!leftRight)
+        iconPosition.x = MARGIN_BORDER;
     else
-        position.x = WINDOW_WIDTH - (WIDTH_BACKGROUND + MARGIN_BORDER);
-    position.x += MARGIN_BORDER;
+        iconPosition.x = WINDOW_WIDTH - (WIDTH_BACKGROUND + MARGIN_BORDER);
+    iconPosition.x += MARGIN_BORDER;
+    textPosition.x = iconPosition.x + DETAILS_ICON_SIZE * 3;
 
-    texts->add<ComponentText>("assets/CollegiateBlackFLF.ttf",
-        "bombs: " + std::to_string(entity->get<ComponentBombs>()->getNbCurrBombs()) + "/" + std::to_string(entity->get<ComponentBombs>()->getNbMaxBombs())
-            + "\nbomb radius: " + std::to_string(entity->get<ComponentExplodable>()->getBlastRange())
-            + "\nspeed: " + std::to_string(entity->get<ComponentSpeed>()->getSpeed()),
-        position, DETAILS_SIZE, raylib::Color::White());
-    texts->add<ComponentDrawable>(true, false);
-    return (texts);
+    bomb->add<ComponentTexture>("assets/16/bomb_icon.png", iconPosition);
+    bomb->add<ComponentText>("assets/CollegiateBlackFLF.ttf",
+        std::to_string(entity->get<ComponentBombs>()->getNbCurrBombs()) + "/" + std::to_string(entity->get<ComponentBombs>()->getNbMaxBombs()), textPosition,
+        DETAILS_SIZE, raylib::Color::White());
+    bomb->add<ComponentDrawable>(true, false);
+    return (bomb);
+}
+
+static ecs::IEntity *displayRadius(ecs::IEntity *entity, bool topBottom, bool leftRight)
+{
+    ecs::IEntity *bomb = new ecs::IEntity();
+    raylib::Vector2 iconPosition = {MARGIN_BORDER, MARGIN_BORDER};
+    raylib::Vector2 textPosition = {MARGIN_BORDER, MARGIN_BORDER};
+
+    if (!topBottom)
+        iconPosition.y = MARGIN_BORDER;
+    else
+        iconPosition.y = WINDOW_HEIGHT - (HEIGHT_BACKGROUND + MARGIN_BORDER);
+    iconPosition.y += NAME_SIZE * 2 + DETAILS_ICON_SIZE * 3;
+    textPosition.y = iconPosition.y + DETAILS_ICON_SIZE / 2;
+    if (!leftRight)
+        iconPosition.x = MARGIN_BORDER;
+    else
+        iconPosition.x = WINDOW_WIDTH - (WIDTH_BACKGROUND + MARGIN_BORDER);
+    iconPosition.x += MARGIN_BORDER;
+    textPosition.x = iconPosition.x + DETAILS_ICON_SIZE * 3;
+
+    bomb->add<ComponentTexture>("assets/16/radius_icon.png", iconPosition);
+    bomb->add<ComponentText>("assets/CollegiateBlackFLF.ttf", std::to_string(entity->get<ComponentExplodable>()->getBlastRange()), textPosition, DETAILS_SIZE,
+        raylib::Color::White());
+    bomb->add<ComponentDrawable>(true, false);
+    return (bomb);
+}
+
+static ecs::IEntity *displaySpeed(ecs::IEntity *entity, bool topBottom, bool leftRight)
+{
+    ecs::IEntity *bomb = new ecs::IEntity();
+    raylib::Vector2 iconPosition = {MARGIN_BORDER, MARGIN_BORDER};
+    raylib::Vector2 textPosition = {MARGIN_BORDER, MARGIN_BORDER};
+
+    if (!topBottom)
+        iconPosition.y = MARGIN_BORDER;
+    else
+        iconPosition.y = WINDOW_HEIGHT - (HEIGHT_BACKGROUND + MARGIN_BORDER);
+    iconPosition.y += NAME_SIZE * 2 + DETAILS_ICON_SIZE * 6;
+    textPosition.y = iconPosition.y + DETAILS_ICON_SIZE / 2;
+    if (!leftRight)
+        iconPosition.x = MARGIN_BORDER;
+    else
+        iconPosition.x = WINDOW_WIDTH - (WIDTH_BACKGROUND + MARGIN_BORDER);
+    iconPosition.x += MARGIN_BORDER;
+    textPosition.x = iconPosition.x + DETAILS_ICON_SIZE * 3;
+
+    bomb->add<ComponentTexture>("assets/16/speed_icon.png", iconPosition);
+    bomb->add<ComponentText>(
+        "assets/CollegiateBlackFLF.ttf", std::to_string(entity->get<ComponentSpeed>()->getSpeed()), textPosition, DETAILS_SIZE, raylib::Color::White());
+    bomb->add<ComponentDrawable>(true, false);
+    return (bomb);
 }
 
 void displayInformations(ecs::Core &core)
@@ -123,8 +178,8 @@ void displayInformations(ecs::Core &core)
     std::size_t count = 0;
     ecs::IEntity *players = new ecs::IEntity();
     ecs::IEntity *bots = new ecs::IEntity();
-    bool TopBottom = false;
-    bool LeftRight = false;
+    bool topBottom = false;
+    bool leftRight = false;
 
     players->add<ComponentBombs>(4);
     bots->add<ComponentBombs>(4);
@@ -141,17 +196,19 @@ void displayInformations(ecs::Core &core)
     for (auto &entity : entities) {
         if (entity->has<ComponentBombs>()) { //// Change to ComponentHealth
             // It's a player/bot
-            TopBottom = count / 2;
-            LeftRight = count % 2;
-            core.addEntity(createBackground(TopBottom, LeftRight, playersColor[count]));
-            core.addEntity(displayPlayerHeader(count, TopBottom, LeftRight));
+            topBottom = count / 2;
+            leftRight = count % 2;
+            core.addEntity(createBackground(topBottom, leftRight, playersColor[count]));
+            core.addEntity(displayPlayerHeader(count, topBottom, leftRight));
             if (entity->has<ComponentControllable>()) {
                 // It's a player
             } else {
                 // It's a bot
-                core.addEntity(displayBotIcon(TopBottom, LeftRight));
+                core.addEntity(displayBotIcon(topBottom, leftRight));
             }
-            core.addEntity(displayTexts(entity, TopBottom, LeftRight));
+            core.addEntity(displayBombs(entity, topBottom, leftRight));
+            core.addEntity(displayRadius(entity, topBottom, leftRight));
+            core.addEntity(displaySpeed(entity, topBottom, leftRight));
             count++;
         }
     }

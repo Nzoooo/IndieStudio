0f22a4b3eb28d0c685ae747adae012ab36840cb3
BELLICHA Tanguy <tanguy.bellicha@epitech.eu>
2022-06-15 12:16:36 +0200

    [UPT] Remove warning from using deprecated method

--- src/ECS/Components/ComponentButton.cpp
+++ src/ECS/Components/ComponentButton.cpp
@@ -9,7 +9,8 @@
 
 ComponentButton::ComponentButton(raylib::Vector2 pos, std::string buttonText)
 {
-    this->_pos = pos;
+    this->_pos.x = pos.x;
+    this->_pos.y = pos.y;
     this->_rectangleUnactive = new raylib::Rectangle(1611, 242, 358, 117);
     this->_rectangleActive = new raylib::Rectangle(1611, 395, 358, 117);
     this->_texture.Load("assets/menuAsset.png");
@@ -23,7 +24,8 @@ void ComponentButton::Draw()
         this->_texture.Draw(*this->_rectangleActive, this->_pos);
     else
         this->_texture.Draw(*this->_rectangleUnactive, this->_pos);
-    this->_font.DrawText(this->_text, raylib::Vector2(1920 / 2.0f - (this->_text.length() * 20) / 2.0f, this->_pos.y + 40.0), 40.0, 3.0, raylib::Color::White());
+    this->_font.DrawText(
+        this->_text, raylib::Vector2(1920 / 2.0f - (this->_text.length() * 20) / 2.0f, this->_pos.y + 40.0), 40.0, 3.0, raylib::Color::White());
 }
 
 void ComponentButton::setState(bool isActive)

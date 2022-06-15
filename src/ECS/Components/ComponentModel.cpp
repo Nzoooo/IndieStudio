5ca20717d700bd53e24b54c773a353bb36080040
noaleclaire <noa.leclaire@epitech.eu>
2022-06-15 16:16:19 +0200

    [UPD] ComponentModels

--- src/ECS/Components/ComponentModel.cpp
+++ src/ECS/Components/ComponentModel.cpp
@@ -5,12 +5,18 @@
 ** ComponentModel
 */
 
+#include <iostream>
 #include "ComponentModel.hpp"
 
-ComponentModel::ComponentModel(std::string modelPath, raylib::Vector3 pos, std::string animPath, unsigned int animCount) : _animFrameCounter(0)
+ComponentModel::ComponentModel(std::string modelPath, raylib::Vector3 pos, raylib::Vector3 scale,
+    std::string animPath, unsigned int animCount, float rotateAngle,
+    raylib::Vector3 rotateAxis) : _animFrameCounter(0)
 {
     _model.Load(modelPath);
     _pos = pos;
+    _scale = scale;
+    _rotateAxis = rotateAxis;
+    _rotateAngle = rotateAngle;
     _animCount = animCount;
     if (animPath != "")
         _anim.Load(animPath, &_animCount);
@@ -23,7 +29,7 @@ ComponentModel::~ComponentModel()
 
 void ComponentModel::Draw()
 {
-    _model.Draw(_pos, 1.0f, raylib::Color::White());
+    _model.DrawEx(_pos, _rotateAxis, _rotateAngle, _scale, raylib::Color::White());
 }
 
 void ComponentModel::Update(int whichAnim)
@@ -34,4 +40,44 @@ void ComponentModel::Update(int whichAnim)
         if (_animFrameCounter >= _anim.getAnims()[whichAnim].frameCount)
             _animFrameCounter = 0;
     }
+}
+
+raylib::Vector3 ComponentModel::getPos() const
+{
+    return (_pos);
+}
+
+raylib::Vector3 ComponentModel::getScale() const
+{
+    return (_scale);
+}
+
+raylib::Vector3 ComponentModel::getRotateAxis() const
+{
+    return (_rotateAxis);
+}
+
+float ComponentModel::getRotateAngle() const
+{
+    return (_rotateAngle);
+}
+
+void ComponentModel::setPos(raylib::Vector3 pos)
+{
+    _pos = pos;
+}
+
+void ComponentModel::setScale(raylib::Vector3 scale)
+{
+    _scale = scale;
+}
+
+void ComponentModel::setRotateAxis(raylib::Vector3 rotateAxis)
+{
+    _rotateAxis = rotateAxis;
+}
+
+void ComponentModel::setRotateAngle(float rotateAngle)
+{
+    _rotateAngle = rotateAngle;
 }

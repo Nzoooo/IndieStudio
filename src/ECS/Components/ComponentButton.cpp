afec7f5e0215751b5f9574ed4d66bef3b68aef76
noaleclaire <noa.leclaire@epitech.eu>
2022-06-17 17:53:47 +0200

    [UPT] repositionning text on buttons

--- src/ECS/Components/ComponentButton.cpp
+++ src/ECS/Components/ComponentButton.cpp
@@ -16,7 +16,7 @@ ComponentButton::ComponentButton(raylib::Vector2 pos, std::string buttonText, ra
     this->_font.Load("assets/NewAthletic.ttf");
     this->_text = buttonText;
     if (posText.x == -1.0f && posText.y == -1.0f)
-        this->_posText = raylib::Vector2(1920 / 2.0f - (this->_text.length() * 20) / 2.0f, this->_pos.y + 40.0);
+        this->_posText = raylib::Vector2(1920 / 2.0f - (this->_text.length() * 18) / 2.0f, this->_pos.y + 40.0);
     else
         this->_posText = posText;
     this->_fontSize = fontSize;
@@ -28,8 +28,7 @@ void ComponentButton::Draw()
         this->_texture.Draw(*this->_rectangleActive, this->_pos);
     else
         this->_texture.Draw(*this->_rectangleUnactive, this->_pos);
-    this->_font.DrawText(
-        this->_text, raylib::Vector2(1920 / 2.0f - (this->_text.length() * 18) / 2.0f, this->_pos.y + 40.0), 40.0, 3.0, raylib::Color::White());
+    this->_font.DrawText(this->_text, this->_posText, this->_fontSize, 3.0, raylib::Color::White());
 }
 
 void ComponentButton::setState(bool isActive)

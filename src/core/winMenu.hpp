96239256450c220e856739a0a4a2d45a0c249121
Louis <louis.maestre@epitech.eu>
2022-06-19 20:49:41 +0200

    [UPDATE] add winMenu file

---
+++ src/core/winMenu.hpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2022
+** B-YEP-400-NAN-4-1-indiestudio-matthis.lesur
+** File description:
+** winMenu
+*/
+
+#pragma once
+
+#include "ECS/ecs.hpp"
+#include "raylib/include/Color.hpp"
+#include "raylib/include/Font.hpp"
+#include "raylib/include/Model.hpp"
+#include "raylib/include/Mouse.hpp"
+#include "raylib/include/Rectangle.hpp"
+#include "raylib/include/Texture.hpp"
+#include "raylib/include/Vector2.hpp"
+#include "raylib/include/Vector3.hpp"
+#include "raylib/include/Window.hpp"
+
+ecs::Scenes winMenu(std::string winner);
+
+/* !WINMENU_HPP_ */
